54ad556c375e238c65b4570653f1d93456150422
Syawqi Muhammad <syawqi.zone@gmail.com>
2020-10-30 17:25:30 +0800

    .

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:beacon_broadcast/beacon_broadcast.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainApp());
+
+
+class MainApp extends StatefulWidget {
+  MainApp({Key key}) : super(key: key);
+
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
